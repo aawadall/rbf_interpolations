35ef541c2780676024ba24a8c5ef5442383ad960
Ahmed Awadallah <aawadall@ualberta.ca>
2023-02-09 13:42:01 -0500

    implemented steepest descent but not used yet

--- RadialBasisInterpolation/src/golang/rbfinterp/utils/gradient_descent.go
+++ RadialBasisInterpolation/src/golang/rbfinterp/utils/gradient_descent.go
@@ -2,7 +2,6 @@ package utils
 
 import (
 	"fmt"
-	"math"
 )
 
 // GradientDescent
@@ -47,74 +46,4 @@ func Objective(A [][]float64, y []float64, theta []float64) float64 {
 	J := L2Norm(errVector)
 
 	return J
-}
-
-func L2Norm(vector []float64) float64 {
-	J := 0.0
-	for i := 0; i < len(vector); i++ {
-		J += math.Pow(vector[i], 2.0)
-	}
-	return J
-}
-
-func MatVecDot(A [][]float64, theta []float64) []float64 {
-	y_hat := make([]float64, len(A))
-	for i := 0; i < len(A); i++ {
-		for j := 0; j < len(A[0]); j++ {
-			y_hat[i] += A[i][j] * theta[j]
-		}
-	}
-	return y_hat
-}
-
-// Gradient
-func Gradient(A [][]float64, y []float64, theta []float64) []float64 {
-	// Given dense matrix A, vector y, and vector theta
-	// Gradient g = -2 * A^T * (y - A * theta)
-	// Calculate y_hat as A * theta
-	y_hat := MatVecDot(A, theta)
-
-	// calculate diff as y - y_hat
-	diff := make([]float64, len(y))
-	for i := 0; i < len(y); i++ {
-		diff[i] = y[i] - y_hat[i]
-	}
-
-	// Calculate A transpose
-	A_transpose := Transpose(A)
-
-	// Calculate A transpose * diff
-	ATdiff := MatVecDot(A_transpose, diff)
-
-	// Calculate -2 * A transpose * diff
-	return SclaerVecMult(-2.0, ATdiff)
-}
-
-// Transpose
-func Transpose(A [][]float64) [][]float64 {
-	A_transpose := make([][]float64, len(A[0]))
-	for i := 0; i < len(A[0]); i++ {
-		A_transpose[i] = make([]float64, len(A))
-		for j := 0; j < len(A); j++ {
-			A_transpose[i][j] = A[j][i]
-		}
-	}
-	return A_transpose
-}
-
-// SclaerVecMult
-func SclaerVecMult(scalar float64, vector []float64) []float64 {
-	for i := 0; i < len(vector); i++ {
-		vector[i] *= scalar
-	}
-	return vector
-}
-
-// DotProduct
-func DotProduct(x, y []float64) float64 {
-	dot := 0.0
-	for i := 0; i < len(x); i++ {
-		dot += x[i] * y[i]
-	}
-	return dot
 }
---
+++ RadialBasisInterpolation/src/golang/rbfinterp/utils/steepest_descent.go
@@ -0,0 +1,61 @@
+package utils
+
+// SteepestDescent
+func SteepestDescent(A [][]float64, y []float64, theta []float64, alpha float64, max_iter int, epsilon float64) []float64 {
+	// Initialize theta to 0 vector
+	// theta := make([]float64, len(A[0]))
+
+
+	// TODO - Implement Steepest Descent Algorithm
+
+	// 1. initialize theta to initial guess
+	result_theta := theta
+	
+	// 2. calculate gradient using this theta
+	gradient := Gradient(A, y, result_theta)
+
+	iter := 0
+	for L2Norm(gradient) < epsilon && iter < max_iter {
+		iter += 1
+		// 3. get direction 
+		direction := SclaerVecMult(-1.0, gradient)
+
+		// 4. get step size 
+		step_size := LineSearch(A, y, result_theta, direction, alpha)
+
+		// 5. update theta
+		result_theta = VecVecAdd(result_theta, SclaerVecMult(step_size, direction))
+
+		// 6. calculate gradient using this theta
+		gradient = Gradient(A, y, result_theta)
+	}
+	return theta
+}
+
+// LineSearch
+func LineSearch(A [][]float64, y []float64, theta []float64, direction []float64, alpha float64) float64 {
+	// find optimal step size using backtracking line search
+	// 1. initialize step size to 1
+	step_size := 1.0
+
+	// 2. iterate until stop converging
+	for !Converging(A, y, theta, direction, step_size, alpha) {
+		// 3. update step size
+		step_size *= alpha
+	}
+	return step_size
+}
+
+// Converging
+func Converging(A [][]float64, y []float64, theta []float64, direction []float64, step_size float64, alpha float64) bool {
+	// 1. calculate theta_new
+	theta_new := VecVecAdd(theta, SclaerVecMult(step_size, direction))
+
+	// 2. calculate gradient using theta_new
+	gradient_new := Gradient(A, y, theta_new)
+
+	// 3. calculate gradient using theta
+	gradient := Gradient(A, y, theta)
+
+	return L2Norm(gradient_new) < L2Norm(gradient)
+}
